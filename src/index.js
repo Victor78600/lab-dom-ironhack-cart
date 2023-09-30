84e0785ecc07145f7862894162041aba8394744f
Victor Duveau <vic.duveau@hotmail.fr>
2023-09-30 17:36:03 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  // console.log("Calculating subtotal, yey!");
+  const price = product.querySelector(".price span").textContent;
+  const quantity = product.querySelector(".quantity input").value;
+  // console.log(price, quantity);
+  let subtotal = price * quantity;
+  // console.log(subtotal);
+  let textSubtotal = product.querySelector(".subtotal span");
+  textSubtotal.textContent = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
+  let arrSubtotal = [];
+  const allProduct = document.querySelectorAll(".product");
+  for (const element of allProduct) {
+    //   console.log(element);
+    updateSubtotal(element);
+    let subtotal = updateSubtotal(element);
+    arrSubtotal.push(subtotal);
+    // console.log(arrSubtotal);
+    // return arrSubtotal;
+  }
+  const totalScore = arrSubtotal.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  // console.log(totalScore);
+  let scroreFinal = document.querySelector("#total-value span");
+  // console.log(scroreFinal);
+  scroreFinal.textContent = totalScore;
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
   // ITERATION 3
-  //... your code goes here
+  // let subtotal = document.querySelector(".subtotal span").textContent;
+  // console.log(textSubtotal.textContent);
+  // let arrProd = [];
+  // const allProduct = arrProd.filter((prod) => prod.class === "product");
+  // arrProd.push(allProduct);
+  // console.log(arrProd[i]);
+  // updateSubtotal(allProduct);
+  // end of test
+  // allProduct.forEach((productRow) => {
+  //   updateSubtotal(productRow);
+  // });
+  // ITERATION 2
+  // const products = document.querySelectorAll(".product");
+  // for (const element of arrProd) {
+  // let arrProd = [];
+  // for (let i = 0; i < arrProd.length; i++) {
+  // const element = arrProd[i];
+  //   updateSubtotal(products[i]);
+  // updateSubtotal(element);
+  // console.log(arrProd[i]);
+  // let subTotal = element.querySelector(".subtotal span").innerHTML;
+  // arrProd.push(subTotal);
+  //   console.log(arrProd);
 }
+// }
 
+calculateAll();
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  console.log("The target in remove is:", target);
   //... your code goes here
 }
 
@@ -34,9 +74,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
   //... your code goes here
 });
